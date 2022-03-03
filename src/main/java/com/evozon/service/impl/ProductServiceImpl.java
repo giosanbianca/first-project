bf363d208a66bff4ab63bed4cb0d723c9870d9d6
andrei <andrei_matei59@yahoo.com>
2022-03-03 17:26:55 +0200

    Add version with java-based configuration

--- src/main/java/com/evozon/service/impl/ProductServiceImpl.java
+++ src/main/java/com/evozon/service/impl/ProductServiceImpl.java
@@ -4,10 +4,16 @@ import com.evozon.model.Product;
 import com.evozon.repository.ProductRepository;
 import com.evozon.repository.impl.ProductRepositoryImpl;
 import com.evozon.service.ProductService;
+import org.springframework.stereotype.Component;
+
 
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository = new ProductRepositoryImpl();
+    private ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public Product getById(String id) {
         return productRepository.getById(id);
