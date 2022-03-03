cbffbcde363233880a6e581e4154a5c917d2aa5a
Amos Macintosh <amos.andreica@stud.ubbcluj.ro>
2022-03-03 15:54:43 +0200

    Deleted all "new" by using spring beans.

--- src/main/java/com/evozon/service/impl/ProductServiceImpl.java
+++ src/main/java/com/evozon/service/impl/ProductServiceImpl.java
@@ -7,7 +7,11 @@ import com.evozon.service.ProductService;
 
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository = new ProductRepositoryImpl();
+    private ProductRepository productRepository;
+
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public Product getById(String id) {
         return productRepository.getById(id);
