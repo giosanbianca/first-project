806a12dbacdfeb89fe6a7e7318134c737f86fb9a
916-Silasi-Marius <72075809+916-Silasi-Marius@users.noreply.github.com>
2022-03-03 16:02:45 +0200

    xml version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import com.evozon.model.Cart;
 import com.evozon.service.CartService;
 import com.evozon.service.impl.CartServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        CartService cartService = new CartServiceImpl();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+        CartService cartService = context.getBean("cartService", CartServiceImpl.class);
         cartService.add("123");
         Cart cart = cartService.get();
         System.out.println(cart.getProducts().get(0).getId());
